cbcc5dd78c9f398de23cf02b6883997221e9d0f5
uuganbayar <d.uuganbayar.stu@gmail.com>
2023-07-23 18:09:32 +0800

    config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,22 +1,16 @@
-// path: ./config/env/production/database.js
-
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-  
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "mysql",
     connection: {
-      client: 'postgres',
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: { rejectUnauthorized: false },
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
       },
-      debug: false,
     },
-  }
-};
+    debug: false,
+  },
+});
