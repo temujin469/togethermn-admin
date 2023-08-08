ff62ab57998720549ae432de43dfd75debed8555
uuganbayar <d.uuganbayar.stu@gmail.com>
2023-08-08 21:40:19 +0800

    fixe

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,14 +1,7 @@
 module.exports = [
   "strapi::errors",
   "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      header: "*",
-      origin:"*",
-    },
-  },
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
